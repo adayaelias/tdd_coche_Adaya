fb7cbf60c80b72b128d77eeaa69d9bef43fbf1e2
adaya-elias-gracia <adaeligra@alu.edu.gva.es>
2023-03-31 12:11:35 +0200

    Segundo test check. He modificado el nombre del test, he usado assertion y creado la variable velocidad en la clase coche.

--- .idea/test/testCoche.java
+++ .idea/test/testCoche.java
@@ -1,9 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class testCoche {
     @Test
-    public void test_crear_coche(){
+    public void test_crear_coche_velocidad_cero(){
         Coche nuevoCoche = new Coche();
-
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
