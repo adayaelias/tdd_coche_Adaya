37f712537d3a30614fb458a53460823e56ec7ef2
adaya-elias-gracia <adaeligra@alu.edu.gva.es>
2023-03-31 18:21:23 +0200

    Rama nueva, refacotrizado. He cambiado en nombre de los métodos añadiendo mi nombre.

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerarAdayaElias(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerarAadayaElias(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad < 0)
             velocidad = 0;
